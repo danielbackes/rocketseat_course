1d4a451dd855eb267739ecfef8befbc36f44cdf9
Daniel Backes <danielbackes@protonmail.com>
2020-06-14 17:19:34 -0400

    Configure webpack to build css and image; Create a React state that was obtained from backend using Axios module.

--- gostack_11/nodejs_projects/src/index.js
+++ gostack_11/nodejs_projects/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { uuid } = require('uuidv4');
 
 const log = require('./middleware/log');
@@ -6,6 +7,7 @@ const validateId = require('./middleware/validateId');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(log);
--- gostack_11/reactjs_projects/src/App.js
+++ gostack_11/reactjs_projects/src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      "title": `Novo projeto ${Date.now()}`,
+      "owner": "Daniel Backes"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
+      <Header title="Homepage" />
+
+      <img width={300} src={backgroundImage} />
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
